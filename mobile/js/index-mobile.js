5a41ee16a2f559467d48cf6baf0b039c5f680980
Tommy <tooto1985@gmail.com>
2016-05-17 17:40:41 +0800

    modify

--- mobile/js/index-mobile.js
+++ mobile/js/index-mobile.js
@@ -2,21 +2,22 @@ $(function() {
 	var isTest = true;
 	var isLocal = /^file\:\/\/\//i.test(location.href);
 	(function() {
+		var $rotate = $(".rotate");
 		function dot() {
-			$(".rotatedot>span.active").removeClass();
-			$(".rotatedot>span").eq($(".rotate>a:first>img").attr("alt")).addClass("active");
+			$(".navi>span.active").removeClass();
+			$(".navi>span").eq($(".rotate>a:first>img").attr("alt")).addClass("active");
 		}
 		function next(img,url,index,hasEffect) {
 			var link = "<a href=\"" + url + "\"><img src=\"" + img + "\" width=\"100%\" alt=\"" + index + "\"></a>";
 			if (hasEffect) {
-				$(".rotate>a").first().fadeOut("slow",function() {
-					$(".rotate").append(link);
+				$rotate.find(">a").first().fadeOut("slow",function() {
+					$rotate.append(link);
 					$(this).remove();
 					dot();
 				});
 			} else {
-				$(".rotate").append(link);
-				$(".rotate>a").first().remove();
+				$rotate.append(link);
+				$rotate.find(">a").first().remove();
 				dot();
 			}
 		}
@@ -24,14 +25,14 @@ $(function() {
 			(function() {
 				var html="";
 				for (var i=0,max=data.length;i<max;i++) {
-					$(".rotatepreload").append("<img src=\"" + data[i][0] + "\">");
+					$(".preload").append("<img src=\"" + data[i][0] + "\">");
 					html+="<span";
 					if (i===0) {
 						html+= " class=\"active\"";
 					}
 					html+="></span>";
 				}
-				$(".rotatedot").html(html);
+				$(".navi").html(html);
 			})();
 			(function() {
 				var i=0;
@@ -46,11 +47,11 @@ $(function() {
 				i++;
 				next(data[i][0],data[i][1],i);
 				i++;
-				$(".rotatedot>span").click(function() {
+				$(".navi>span").click(function() {
 					var nowat = parseInt($(".rotate>a:first>img").attr("alt")),
 						index = $(this).index();
 					if (nowat !== index) {
-						$(".rotate>a:last").attr("href",data[index][1]).find(">img").attr({src:data[index][0],alt:index});
+						$rotate.find(">a").last().attr("href",data[index][1]).find(">img").attr({src:data[index][0],alt:index});
 						next(data[index][0],data[index][1],index,true);
 						i = index;
 					}
@@ -60,22 +61,22 @@ $(function() {
 				var isTouch = false,
 					move = 0;
 				function prev() {
-					$(".rotatedot>span.active").prev().click();
+					$(".navi>span.active").prev().click();
 				}
 				function next() {
-					$(".rotatedot>span.active").next().click();
+					$(".navi>span.active").next().click();
 				}
                 if ("ontouchstart" in document.documentElement) {
                     isTouch = true;
                 }
-                $(".rotate").on(isTouch ? "touchstart" : "mousedown", "img" , function (e) {
+                $rotate.on(isTouch ? "touchstart" : "mousedown", "img" , function (e) {
                     if (e.type === "mousedown") {
                         move = e.pageX;
                     } else {
                         move = e.originalEvent.touches[0].pageX;
                     }
                 });
-                $(".rotate").on(isTouch? "touchmove": "mousemove", "img", function (e) {
+                $rotate.on(isTouch? "touchmove": "mousemove", "img", function (e) {
                     var target;
                     if (e.type === "mousemove") {
                         target = e.pageX;
@@ -99,7 +100,15 @@ $(function() {
 			$.getJSON("../../../../common/customhtml.ashx?code=CacoAdRotator", getdata);
 		}
 	})(); //rotate
-
-
-
+	(function() {
+		function getdata(data) {
+			console.log(data.length);
+			//todo
+		}
+		if (isLocal || isTest) {
+			getdata([["\thttp://123.sweesa.com/new/left01.jpg","\thttp://123.sweesa.com/new/left01-1.jpg",""],["\thttp://123.sweesa.com/new/left02.jpg","\thttp://123.sweesa.com/new/left02-1.jpg","\thttps://www.facebook.com/SWEESA.tw/"],["\thttp://456.sweesa.com/medium/left03.jpg","\thttp://456.sweesa.com/medium/left03-1.jpg","http://www.sweesa.com/Shop/itemList.aspx?m=22&p=178&smfp=1&&o=4&sa=1"]]);
+		} else {
+			$.getJSON("../../../../common/customhtml.ashx?code=CacoAdSelector", getdata);
+		}
+	})(); //tips
 });
