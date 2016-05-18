e76e4c4ee294979dd038553eb12551bb339a3b5c
Tommy <tooto1985@gmail.com>
2016-05-18 17:20:27 +0800

    modify event

--- mobile/js/index-mobile.js
+++ mobile/js/index-mobile.js
@@ -36,13 +36,14 @@ $(function() {
 			})();
 			(function() {
 				var i=0;
-				setInterval(function() {
+				function run() {
 					if (i>=data.length) {
 						i=0;
 					}
 					next(data[i][0],data[i][1],i,true);
 					i++;
-				},2000);
+				}
+				var sid = setInterval(run,2000);
 				next(data[i][0],data[i][1],i);
 				i++;
 				next(data[i][0],data[i][1],i);
@@ -56,6 +57,13 @@ $(function() {
 						i = index;
 					}
 				});
+
+				$rotate.add(".navi>span").hover(function() {
+					clearInterval(sid);
+				},function() {
+					sid = setInterval(run,2000);
+				});
+
 			})();
 			(function() {
 				var isTouch = false,
@@ -75,6 +83,7 @@ $(function() {
                     } else {
                         move = e.originalEvent.touches[0].pageX;
                     }
+                    e.stopPropagation();
                 });
                 $rotate.on(isTouch? "touchmove": "mousemove", "img", function (e) {
                     var target;
@@ -91,6 +100,7 @@ $(function() {
                         }
                         move = 0;
                     }
+                    e.stopPropagation();
                 });
 			})();
 		}
@@ -102,11 +112,62 @@ $(function() {
 	})(); //rotate
 	(function() {
 		function getdata(data) {
-			console.log(data.length);
-			//todo
+			var $outbox = $(".index>.event>.outbox"),
+				$inbox = $(".index>.event>.outbox>.inbox"),
+				html="";
+			for(var i=0,max=data.length;i<max;i++) {
+				html+="<a href=\"" + data[i][2] + "\"><img src=\"" + data[i][0] + "\"></a>";
+			}
+			$inbox.html(html);
+			$(window).resize(function() {
+				$inbox.find(">a").width(Math.floor($outbox.width()/2));
+				$inbox.width(Math.floor($outbox.width()/2)*data.length);
+			}).resize();
+			function next() {
+				if (!$inbox.is(":animated")) {
+					var inboxWidth = $inbox.width();
+					var left = parseInt($inbox.css("left"));
+					var width = $outbox.width();
+					if (0-inboxWidth+width>=left) {
+						left = 0;
+					} else {
+						if (inboxWidth+left-width>=width) {
+							left = left - width;	
+						} else {
+							left = left - (inboxWidth+left-width);
+						}
+					}
+					$inbox.animate({left:left});
+				}
+			}
+			function back() {
+				if (!$inbox.is(":animated")) {
+					var inboxWidth = $inbox.width();
+					var left = parseInt($inbox.css("left"));
+					var width = $outbox.width();
+					if (left>=0) {
+						left = left - (inboxWidth+left-width);
+					} else {
+						if (left+width<=0) {
+							left = left + width;
+						} else {
+							left = 0;
+						}
+					}
+					$inbox.animate({left:left});
+				}
+			}
+			var sid = setInterval(next,3000);
+			$(".index>.event>.flagright").click(next);
+			$(".index>.event>.flagleft").click(back);
+			$(".index>.event>.outbox>.inbox,.index>.event>.flagright,.index>.event>.flagleft").hover(function() {
+				clearInterval(sid);
+			},function() {
+				sid = setInterval(next,3000);
+			});
 		}
 		if (isLocal || isTest) {
-			getdata([["\thttp://123.sweesa.com/new/left01.jpg","\thttp://123.sweesa.com/new/left01-1.jpg",""],["\thttp://123.sweesa.com/new/left02.jpg","\thttp://123.sweesa.com/new/left02-1.jpg","\thttps://www.facebook.com/SWEESA.tw/"],["\thttp://456.sweesa.com/medium/left03.jpg","\thttp://456.sweesa.com/medium/left03-1.jpg","http://www.sweesa.com/Shop/itemList.aspx?m=22&p=178&smfp=1&&o=4&sa=1"]]);
+			getdata([["\thttp://123.sweesa.com/new/left01.jpg","\thttp://123.sweesa.com/new/left01-1.jpg",""],["\thttp://123.sweesa.com/new/left02.jpg","\thttp://123.sweesa.com/new/left02-1.jpg","\thttps://www.facebook.com/SWEESA.tw/"],["\thttp://456.sweesa.com/medium/left03.jpg","\thttp://456.sweesa.com/medium/left03-1.jpg","http://www.sweesa.com/Shop/itemList.aspx?m=22&p=178&smfp=1&&o=4&sa=1"],["http://www.7iaoshou.com/products_thumbnail_img/234/1454046883905.jpg","",""],["http://pic.kximg.cc/pic/app/44/46/1194_171444636_rating-366x355.jpeg","",""],["http://i2.wp.com/www.mydesy.com/wp-content/uploads/2014/05/50-Tree-Houses10.jpg?fit=750%2C355","",""],["http://ecx.images-amazon.com/images/I/51M62w7GXGL._SY355_.jpg","",""]]);
 		} else {
 			$.getJSON("../../../../common/customhtml.ashx?code=CacoAdSelector", getdata);
 		}
